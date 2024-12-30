c0775466c1b5207fbfb267c7058642441db38e55
Vivek Singh <rajputvivek649@gmail.com>
2024-12-30 17:09:29 +0530

    dyanamic data changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    main: './assets/js/main.js',
-    dynamic: './assets/js/dynamicdata.js',
-    jquery: './assets/js/vendor/jquery.min.js',
-    app: '/app.js',
+    main: "./assets/js/main.js",
+    jquery: "./assets/js/vendor/jquery.min.js",
+    dynamic: "./assets/js/dynamicdata.js",
+    app: "/app.js",
     styles: [
-        './assets/css/vendor/bootstrap.min.css',
-      './assets/css/style.css',
-      './assets/css/style-2.css',
-    ]
+      "./assets/css/vendor/bootstrap.min.css",
+      "./assets/css/style.css",
+      "./assets/css/style-2.css",
+    ],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].bundle.js",
     clean: true,
   },
   optimization: {
@@ -35,7 +35,7 @@ module.exports = {
       new CssMinimizerPlugin(),
     ],
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
       name: false,
     },
   },
@@ -46,31 +46,31 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              url: false // This will prevent css-loader from processing url() statements
-            }
-          }
+              url: false, // This will prevent css-loader from processing url() statements
+            },
+          },
         ],
       },
       {
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'assets/[name][ext]'
-        }
-      }
+          filename: "assets/[name][ext]",
+        },
+      },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/[name].bundle.css',
+      filename: "css/[name].bundle.css",
     }),
   ],
   resolve: {
     fallback: {
-      "path": false,
-      "fs": false
-    }
-  }
+      path: false,
+      fs: false,
+    },
+  },
 };
